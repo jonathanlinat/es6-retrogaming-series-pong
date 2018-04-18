3f101e1a077396dccf8f30fa50da1c8d5eca2848
Jonathan Linat <jonathan.linat@globant.com>
2018-04-18 13:01:01 -0300

    Initial commit.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,43 @@
-import log from "./modules/js/helpers/log"
+const canvas = document.getElementById("pong")
+const context = canvas.getContext("2d")
 
-const consoleMessage = (bool) => {
-  if (bool) log("Yet Another And Really Basic Webpack ES6 Starterkit")
+class Vect {
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
 }
 
-consoleMessage(true)
+class Rect {
+  constructor(w, h) {
+    this.pos = new Vect;
+    this.size = new Vect(w, h);
+  }
+}
+
+class Ball extends Rect {
+  constructor() {
+    super(10, 10);
+    this.vel = new Vect;
+  }
+}
+
+class Player extends Rect {
+  constructor() {
+    super(25, 200);
+  }
+}
+
+const ball = new Ball;
+ball.pos.x = 100;
+ball.pos.y = 50;
+
+context.fillStyle = "white"
+context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y)
+
+const player = new Player;
+player.pos.x = 0;
+player.pos.y = 0;
+
+context.fillStyle = "white"
+context.fillRect(player.pos.x, player.pos.y, player.size.x, player.size.y)

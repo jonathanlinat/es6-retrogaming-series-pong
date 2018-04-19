cc60cb7a2a6d58e193bd77b60e16eb15fe3423d8
Jonathan Linat <jonathan.linat@globant.com>
2018-04-19 11:42:12 -0300

    Created basic Pong loop with Ball and collision logic.

--- src/app.js
+++ src/app.js
@@ -1,43 +1,78 @@
-const canvas = document.getElementById("pong")
-const context = canvas.getContext("2d")
+const canvas = document.getElementById("es6-retrogaming-series-pong")
 
 class Vect {
-  constructor(x = 0, y = 0) {
-    this.x = x;
-    this.y = y;
+  constructor(x, y) {
+    this.x = x || 0
+    this.y = y || 0
   }
 }
 
 class Rect {
   constructor(w, h) {
-    this.pos = new Vect;
-    this.size = new Vect(w, h);
+    this.pos = new Vect
+    this.size = new Vect(w, h)
   }
 }
 
 class Ball extends Rect {
   constructor() {
-    super(10, 10);
-    this.vel = new Vect;
+    super(8, 8)
+    this.vel = new Vect()
+  }
+
+  get top() { return this.pos.y - this.size.y / 2 }
+  get right() { return this.pos.x + this.size.x / 2 }
+  get bottom() { return this.pos.y + this.size.y / 2 }
+  get left() { return this.pos.x - this.size.x / 2 }
+}
+
+class Pong {
+  constructor(canvas) {
+    this._canvas = canvas
+    this._context = this._canvas.getContext("2d")
+
+    this.ball = new Ball
+    this.ball.pos.x = (this._canvas.width / 2) + this.ball.left
+    this.ball.pos.y = (this._canvas.height / 2) + this.ball.top
+    this.ball.vel.x = 100
+    this.ball.vel.y = 100
+
+    let lastTime
+    const callback = (millis) => {
+      if (lastTime) this.update((millis - lastTime) / 1000)
+      lastTime = millis
+      requestAnimationFrame(callback)
+    }
+
+    callback()
+  }
+
+  drawRect(rect) {
+    this._context.fillStyle = "white"
+    this._context.fillRect(rect.pos.x, rect.pos.y, rect.size.x, rect.size.y)
+  }
+
+  clearRect(rect) {
+    this._context.save()
+    this._context.clearRect(rect.pos.x - 1, rect.pos.y - 1, rect.size.x + 2, rect.size.y + 2)
+    this._context.restore()
+  }
+
+  collide(rect, obj) {
+    if (rect.left < 0 || rect.right > obj.width) rect.vel.x = -rect.vel.x
+    if (rect.top < 0 || rect.bottom > obj.height) rect.vel.y = -rect.vel.y
+  }
+
+  update(time) {
+    this.clearRect(this.ball)
+    
+    this.ball.pos.x += this.ball.vel.x * time
+    this.ball.pos.y += this.ball.vel.y * time
+
+    this.collide(this.ball, this._canvas)
+  
+    this.drawRect(this.ball, this._canvas)
   }
 }
 
-class Player extends Rect {
-  constructor() {
-    super(25, 200);
-  }
-}
-
-const ball = new Ball;
-ball.pos.x = 100;
-ball.pos.y = 50;
-
-context.fillStyle = "white"
-context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y)
-
-const player = new Player;
-player.pos.x = 0;
-player.pos.y = 0;
-
-context.fillStyle = "white"
-context.fillRect(player.pos.x, player.pos.y, player.size.x, player.size.y)
+const pong = new Pong(canvas)

442f6f137e84c13fc880b97a1eeb1fb14c39740f
Jonathan Linat <jonathan.linat@globant.com>
2018-07-12 22:20:46 -0400

    Added sound effects on collision.

--- src/modules/logic/collision.js
+++ src/modules/logic/collision.js
@@ -3,6 +3,8 @@ import Sound from '../utils/sound'
 export default class Collision {
   constructor () {
     this.sound = new Sound()
+
+    this.sound.disable()
   }
 
   detect (collider = {}, collided = {}) {
@@ -14,11 +16,11 @@ export default class Collision {
       }
     } else if (collider.name === 'ball' && collided.name === 'canvas') {
       if (collider.left < 32 || collider.right > (collided.width - 32)) {
-        this.sound.generate('square', 257, 490)
         collider.velocityX = -collider.velocityX
+        this.sound.generate('square', 257, 490)
       } else if (collider.top < 0 || collider.bottom > collided.height) {
-        this.sound.generate('square', 16, 226)
         collider.velocityY = -collider.velocityY
+        this.sound.generate('square', 16, 226)
       }
     } else if (collider.name === 'ball' && collided.name === 'player') {
       if (collider.right > collided.left && collider.left < collided.right && collider.bottom > collided.top && collider.top < collided.bottom) {
@@ -26,15 +28,15 @@ export default class Collision {
           collider.velocityX = -collider.velocityX
           const compoundedVelocity = (collider.positionY - collided.positionY) * (collided.positionY - collider.positionY) * 1.5
           if (collider.positionY < collided.positionY) {
-            this.sound.generate('square', 96, 459)
             collider.velocityY = compoundedVelocity
           } else if (collider.positionY > collided.positionY) {
-            this.sound.generate('square', 96, 459)
             collider.velocityY = -compoundedVelocity
           }
-        } else if (collider.bottom > collided.top && collider.top < collided.bottom) {
+        } else if (collider.top > collided.bottom && collider.bottom < collided.top) {
+          collider.velocityX = -collider.velocityX
           collider.velocityY = -collider.velocityY
         }
+        this.sound.generate('square', 96, 459)
       }
     }
   }
--- src/modules/utils/sound.js
+++ src/modules/utils/sound.js
@@ -1,22 +1,29 @@
 export default class Sound {
   constructor () {
+    this.disabled = false
     this.oscillator = null
     this.gainNode = null
 
     this.audioContext = new AudioContext()
   }
 
+  disable () {
+    this.disabled = true
+  }
+
   generate (type = '', duration = 0, frequency = 0) {
-    this.oscillator = this.audioContext.createOscillator()
-    this.gainNode = this.audioContext.createGain()
+    if (!this.disabled) {
+      this.oscillator = this.audioContext.createOscillator()
+      this.gainNode = this.audioContext.createGain()
 
-    this.oscillator.connect(this.gainNode)
-    this.oscillator.type = type
-    this.oscillator.frequency.value = frequency
+      this.oscillator.connect(this.gainNode)
+      this.oscillator.type = type
+      this.oscillator.frequency.value = frequency
 
-    this.gainNode.connect(this.audioContext.destination)
-    this.oscillator.start(0)
+      this.gainNode.connect(this.audioContext.destination)
+      this.oscillator.start(0)
 
-    this.gainNode.gain.exponentialRampToValueAtTime(0.00001, this.audioContext.currentTime + (duration / 1000))
+      this.gainNode.gain.exponentialRampToValueAtTime(0.00001, this.audioContext.currentTime + (duration / 1000))
+    }
   }
 }
